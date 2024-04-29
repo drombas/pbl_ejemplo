f0a7f91afc0a7143ba0c559e5e5f202b5f4888d2
drombas <50577357+DRomBas@users.noreply.github.com>
2024-04-29 11:55:20 +0200

    Add calculator function

---
+++ calculator.m
@@ -0,0 +1,18 @@
+function result = calculator(a, b, operation)
+    switch operation
+        case 'add'
+            result = a + b;
+        case 'subtract'
+            result = a - b;
+        case 'multiply'
+            result = a * b;
+        case 'divide'
+            if b ~= 0
+                result = a / b;
+            else
+                result = NaN; % Handle division by zero
+            end
+        otherwise
+            disp('Invalid operation');
+    end
+end
