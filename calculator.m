fbe6fad658696444136e590a3aa1d6a708e19794
drombas <50577357+DRomBas@users.noreply.github.com>
2024-04-29 11:57:02 +0200

    quitar operaciones

--- calculator.m
+++ calculator.m
@@ -4,14 +4,6 @@ function result = calculator(a, b, operation)
             result = a + b;
         case 'subtract'
             result = a - b;
-        case 'multiply'
-            result = a * b;
-        case 'divide'
-            if b ~= 0
-                result = a / b;
-            else
-                result = NaN; % Handle division by zero
-            end
         otherwise
             disp('Invalida operation');
     end
